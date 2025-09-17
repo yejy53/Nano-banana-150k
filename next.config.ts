10c9f3f5693084fc98f66f3bc59c7ca575ba3791
Serein <1939342354@qq.com>
2025-09-17 20:55:51 +0800

    fix error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/nano-banana-100k',
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
